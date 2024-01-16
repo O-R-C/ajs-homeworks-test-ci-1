b8d39c280f9d3da99c9271a72bb1ea53b925ba9b
Andrey Semenov <semenov.orc@gmail.com>
2024-01-16 12:36:03 +0300

    add test

---
+++ src/js/__tests__/getHealthStatus.test.js
@@ -0,0 +1,20 @@
+import getHealthStatus from "../getHealthStatus";
+
+const cases = [
+  ["mage", 90, "healthy"],
+  ["warrior", 50, "wounded"],
+  ["warlock", 49, "wounded"],
+  ["druid", 15, "wounded"],
+  ["bard", 10, "critical"],
+  ["kenny", 0, "dead"],
+];
+
+describe("check health status", () => {
+  test.each(cases)(
+    "given %p and %p as arguments, returns %p",
+    (firstArg, secondArg, expectedResult) => {
+      const result = getHealthStatus({ name: firstArg, health: secondArg });
+      expect(result).toBe(expectedResult);
+    }
+  );
+});
--- src/js/getHealthStatus.js
+++ src/js/getHealthStatus.js
@@ -1,5 +1,6 @@
 export default function getHealthStatus(character) {
   if (character.health > 50) return "healthy";
   if (character.health >= 15) return "wounded";
-  return "critical";
+  if (character.health > 0) return "critical";
+  return "dead";
 }
